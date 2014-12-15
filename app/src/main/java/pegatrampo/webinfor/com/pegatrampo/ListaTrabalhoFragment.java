2c1be11b7d8694cc6feedc45f4451bddd59fd1f9
Natanael de Sousa Leite <NATANSL@GMAIL.COM>
2014-12-15 17:12:12 -0200

    Acrescentado o listView de oportunidades

--- app/src/main/java/pegatrampo/webinfor/com/pegatrampo/ListaTrabalhoFragment.java
+++ app/src/main/java/pegatrampo/webinfor/com/pegatrampo/ListaTrabalhoFragment.java
@@ -9,15 +9,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListaTrabalhoFragment extends android.support.v4.app.Fragment {
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_lista_trabalhos, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        ArrayList<ListviewContactItem> listContact = GetlistContact();
+        ListView lv = (ListView)getActivity().findViewById(R.id.lv_contact);
+        lv.setAdapter(new ListviewContactAdapter(getActivity(), listContact));
+
+        return rootView;
+
+        //return inflater.inflate(R.layout.fragment_lista_trabalhos, container, false);
     }
 
+
+    private ArrayList<ListviewContactItem> GetlistContact(){
+        ArrayList<ListviewContactItem> contactlist = new ArrayList<ListviewContactItem>();
+
+        ListviewContactItem contact = new ListviewContactItem();
+
+        contact.SetName("Topher");
+        contact.SetPhone("01213113568");
+        contactlist.add(contact);
+
+        contact = new ListviewContactItem();
+        contact.SetName("Jean");
+        contact.SetPhone("01213869102");
+        contactlist.add(contact);
+
+        contact = new ListviewContactItem();
+        contact.SetName("Andrew");
+        contact.SetPhone("01213123985");
+        contactlist.add(contact);
+
+        return contactlist;
+    }
 }
--- app/src/main/java/pegatrampo/webinfor/com/pegatrampo/MapaTrabalhoFragment.java
+++ app/src/main/java/pegatrampo/webinfor/com/pegatrampo/MapaTrabalhoFragment.java
@@ -4,8 +4,11 @@ package pegatrampo.webinfor.com.pegatrampo;
  * Created by 98287028191 on 10/12/14.
  */
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +24,13 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class MapaTrabalhoFragment extends android.support.v4.app.Fragment {
 
     MapView mMapView;
     private GoogleMap googleMap;
+    private Geocoder geocoder;
 
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,29 +51,54 @@ public class MapaTrabalhoFragment extends android.support.v4.app.Fragment {
 
         googleMap = mMapView.getMap();
         // latitude and longitude
-        double latitude = -20.397833;
-        double longitude = -43.50906;
+        double latitude = -15.780148200000000000;
+        double longitude = -47.929169800000010000;
 
-        // create marker
+        // Vou passar o endereço e o metodo vai retornar a latitude e longitude do endereço para eu setar o marcador no mapa
+        // Link de referencia:
+        // http://pt.stackoverflow.com/questions/31547/como-retornar-latitude-e-longitude-no-android-google-maps
+        // http://stackoverflow.com/questions/472313/android-reverse-geocoding-getfromlocation
+
+        // Parametro de busca do google para resolver esta questão "android mapview geocoder example"
+
+         // Muito bom este link - http://www.botecodigital.info/javascript/introducao-a-api-de-mapas-do-google-maps/
+
+        //geocoder = new Geocoder(this);
+        /*List<Address> enderecos = geocoder.getFromLocationName("Av. Sampaio Vidal, Centro, Marília, SP", 1);
+        if (enderecos.size() > 0) {
+            Log.v("tag", "coordenadas " + enderecos.get(0).getLatitude() + ", " + enderecos.get(0).getLongitude());
+        }
+        */
+
+
+        // create marker(marcador)
         MarkerOptions marker = new MarkerOptions().position(
-                new LatLng(latitude, longitude)).title("Hello Maps");
+                new LatLng(latitude, longitude)).title("Oportunidade de trabalho");
 
         // Changing marker icon
         marker.icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+                .defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
 
         // adding marker
         googleMap.addMarker(marker);
         CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(-20.397833, -43.50906)).zoom(12).build();
-        googleMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(cameraPosition));
+                .target(new LatLng(-15.780148200000000000, -47.929169800000010000)).zoom(12).build();
+
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        //googleMap.animateCamera(CameraUpdateFactory.zoomTo(17),200,null);
+
+        // Setando o tipo de mapa
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Marcando a posição do usuário no mapa
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.setMyLocationEnabled(true);
 
         // Perform any camera updates here
         return v;
-
-
         //return inflater.inflate(R.layout.fragment_mapa_trabalho, container, false);
+
     }
 
     @Override
